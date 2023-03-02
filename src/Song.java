7e76cee926fef4af8e71c0b2bbf09fa8769329be
SypherPK <46838766+TenseUp@users.noreply.github.com>
2023-03-02 08:19:54 +0800

    first upload

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.*;
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners = new ArrayList();
+    public Song(String t, String a){
+        this.title = t;
+        this.artist = a;
+    }
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+    public int howMany(ArrayList<String> arr){
+        ArrayList<String> todays = new ArrayList();
+        for (String i : arr){
+            if (!listeners.contains(i.toLowerCase())){
+                todays.add(i.toLowerCase());
+                listeners.add(i.toLowerCase());}
+        }
+        return todays.size();
+    }
+
 }
